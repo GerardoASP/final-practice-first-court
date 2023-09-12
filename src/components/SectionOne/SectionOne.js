603f94c4dcb8bcf3ecea00098b830431a2f7bec8
GerardoASP <gerardo.sanchezp@autonoma.edu.co>
2023-09-12 14:38:44 -0500

    Achieving the sections

---
+++ src/components/SectionOne/SectionOne.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./SectionOne.scss"
+
+export const SectionOne = () => {
+  return (
+    <div className='SectionOne'>Section One</div>
+  )
+}
---
+++ src/components/SectionThree/SectionThree.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./SectionThree.scss"
+
+export const SectionThree = () => {
+  return (
+    <div className='SectionThree'>Section Three</div>
+  )
+}
